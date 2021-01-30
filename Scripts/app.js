cf3ecb2b0e4674169367ff1e208b4c130c008830
Tomelinski <tomzielinski@live.ca>
2021-01-29 20:36:27 -0500

    addemoved contact class to own js file

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,39 +3,6 @@
 //IIFE - Immediatly invoked function expression
 
 "use strict";
-class Contact {
-  get FullName() {
-    return this.m_fullName;
-  }
-  set FullName(value) {
-    this.m_fullName = value;
-  }
-  get ContactNumber() {
-    return this.m_contactNumber;
-  }
-  set ContactNumber(value) {
-    this.m_contactNumber = value;
-  }
-  get EmailAddress() {
-    return this.m_emailAddress;
-  }
-  set EmailAddress(value) {
-    this.m_emailAddress = value;
-  }
-
-  constructor(fullName, contactNumber, emailAddress) {
-    this.m_fullName = fullName;
-    this.m_contactNumber = contactNumber;
-    this.m_emailAddress = emailAddress;
-  }
-
-  toString() {
-    return `Full Name: ${this.m_fullName} 
-Contact Number: ${this.m_contactNumber}
-Email Address: ${this.m_emailAddress}`;
-  }
-}
-
 (function () {
   function Start() {
     console.log("App started...");
---
+++ Scripts/contact.js
@@ -0,0 +1,35 @@
+"use strict";
+//contact class
+class Contact {
+    //getters and setters
+    get FullName() {
+      return this.m_fullName;
+    }
+    set FullName(value) {
+      this.m_fullName = value;
+    }
+    get ContactNumber() {
+      return this.m_contactNumber;
+    }
+    set ContactNumber(value) {
+      this.m_contactNumber = value;
+    }
+    get EmailAddress() {
+      return this.m_emailAddress;
+    }
+    set EmailAddress(value) {
+      this.m_emailAddress = value;
+    }
+  
+    constructor(fullName, contactNumber, emailAddress) {
+      this.m_fullName = fullName;
+      this.m_contactNumber = contactNumber;
+      this.m_emailAddress = emailAddress;
+    }
+  
+    toString() {
+      return `Full Name: ${this.m_fullName} 
+  Contact Number: ${this.m_contactNumber}
+  Email Address: ${this.m_emailAddress}`;
+    }
+  }
